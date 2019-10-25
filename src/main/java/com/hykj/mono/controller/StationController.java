183ae9418234a4c498f4558ba0f4e4af061fde0f
zlx <1056728977@qq.com>
2019-10-25 19:10:33 +0800

    周立雄第二次提交 1.实体类station的注解完善

--- src/main/java/com/hykj/mono/controller/StationController.java
+++ src/main/java/com/hykj/mono/controller/StationController.java
@@ -5,7 +5,6 @@ import com.hykj.mono.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/hykj/mono/entity/Station.java
+++ src/main/java/com/hykj/mono/entity/Station.java
@@ -1,22 +1,33 @@
 package com.hykj.mono.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("station")
 public class Station {
+    @TableId(value = "id",type = IdType.AUTO)
     private Integer id;
 
     private String title;
 
     private String descr;
 
+    @TableField(value = "guanzhu_user_id")
     private Integer userId;
 
+    @TableField(value = "head_img")
     private String headImg;
 
+    @TableField(value = "background_img")
     private String backgroundImg;
 
+    @TableField(value = "guanzhu_num")
     private Integer focusNum;
 
+    @TableField(value = "type_id")
     private Integer typeId;
 }
